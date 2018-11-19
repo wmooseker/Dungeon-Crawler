92bfd6f6c42c4479941f550989cd900c73030ebb
Mooseker, William Parker <wmooseker@beda-23832l.gonzaga.edu>
2018-11-18 19:51:48 -0800

    scene stuff added

--- Dungeon-Crawler/GameScene.swift
+++ Dungeon-Crawler/GameScene.swift
@@ -11,19 +11,88 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    var entities = [GKEntity]()
-    var graphs = [String : GKGraph]()
+    // constants
+    let waterMaxSpeed: CGFloat = 200
+    let landMaxSpeed: CGFloat = 4000
     
-    private var lastUpdateTime : TimeInterval = 0
-   
+    // if within threshold range of the target, car begins slowing
+    let targetThreshold:CGFloat = 200
     
-    override func sceneDidLoad() {
+    var maxSpeed: CGFloat = 0
+    var acceleration: CGFloat = 0
+    
+    // touch location
+    var targetLocation: CGPoint = .zero
+    var playerCharacter: SKSpriteNode!
 
-        self.lastUpdateTime = 0
+    //let roomRect = CGRect(origin: CGPoint(x: 0, y: 0), size: CGSize(width: 200, height: 200) )
+    
+    
+    func loadSceneNodes() {
+        guard let playerCharacter = childNode(withName: "playerCharacter") as? SKSpriteNode else{
+            fatalError("error with loading player node")
+        }
+        self.playerCharacter = playerCharacter
     }
     
-
+    override func didMove(to view: SKView) {
+        loadSceneNodes()
+        physicsBody = SKPhysicsBody(bodies: <#T##[SKPhysicsBody]#>)
+        maxSpeed = landMaxSpeed
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+        targetLocation = touch.location(in: self)
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+        targetLocation = touch.location(in: self)
+    }
+    
+    
     override func update(_ currentTime: TimeInterval) {
+    }
+    
+    override func didSimulatePhysics() {
+        
+        let offset = CGPoint(x: targetLocation.x - playerCharacter.position.x,
+                             y: targetLocation.y - playerCharacter.position.y)
+        let distance = sqrt(offset.x * offset.x + offset.y * offset.y)
+        let carDirection = CGPoint(x:offset.x / distance,
+                                   y:offset.y / distance)
+        let carVelocity = CGPoint(x: carDirection.x * acceleration,
+                                  y: carDirection.y * acceleration)
+        
+        playerCharacter.physicsBody?.velocity = CGVector(dx: carVelocity.x, dy: carVelocity.y)
+        
+        if acceleration > 5 {
+            playerCharacter.zRotation = atan2(carVelocity.y, carVelocity.x)
+        }
+        
+        // update acceleration
+        // car speeds up to maximum
+        // if within threshold range of the target, car begins slowing
+        // if maxSpeed has reduced due to different tiles,
+        // may need to decelerate slowly to the new maxSpeed
+        
+        if distance < targetThreshold {
+            let delta = targetThreshold - distance
+            acceleration = acceleration * ((targetThreshold - delta)/targetThreshold)
+            if acceleration < 2 {
+                acceleration = 0
+            }
+        } else {
+            if acceleration > maxSpeed {
+                acceleration -= min(acceleration - maxSpeed, 80)
+            }
+            if acceleration < maxSpeed {
+                acceleration += min(maxSpeed - acceleration, 40)
+            }
+        }
         
     }
+    
+  
 }
--- Dungeon-Crawler/GameViewController.swift
+++ Dungeon-Crawler/GameViewController.swift
@@ -23,8 +23,7 @@ class GameViewController: UIViewController {
             if let sceneNode = scene.rootNode as! GameScene? {
                 
                 // Copy gameplay related content over to the scene
-                sceneNode.entities = scene.entities
-                sceneNode.graphs = scene.graphs
+              
                 
                 // Set the scale mode to scale to fit the window
                 sceneNode.scaleMode = .aspectFill
